d1b704a8e8b2bf209d18034a42c71c613897606d
Bruce Sheplan <bruce@sheplan.com>
2024-10-23 14:15:23 -0500

    added nameWidget

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,206 +1,122 @@
-#include "Button.h"
-#include "GlobalTime.h"
-#include "ScreenManager.h"
-#include "Utils.h"
-#include "WidgetSet.h"
-#include "clockwidget/ClockWidget.h"
-#include "config_helper.h"
-#include "icons.h"
-#include "weatherwidget/WeatherWidget.h"
-#include "webdatawidget/WebDataWidget.h"
-#include "wifiwidget/WifiWidget.h"
+#include "core/wifiWidget.h"
+#include "widgetSet.h"
+#include "screenManager.h"
+#include "widgets/clockWidget.h"
+#include "widgets/weatherWidget.h"
+#include "widgets/webDataWidget.h"
+#include "widgets/nameWidget.h"
 #include <Arduino.h>
-
-#ifdef STOCK_TICKER_LIST
-    #include "stockwidget/StockWidget.h"
-#endif
-#ifdef PARQET_PORTFOLIO_ID
-    #include "parqetwidget/ParqetWidget.h"
-#endif
-#ifdef MQTT_WIDGET_HOST
-    #include "mqttwidget/MQTTWidget.h"
-#endif
+#include <Button.h>
+#include <globalTime.h>
+#include <config.h>
+#include <widgets/stockWidget.h>
 
 TFT_eSPI tft = TFT_eSPI();
 
-#ifdef WIDGET_CYCLE_DELAY
-unsigned long m_widgetCycleDelay = WIDGET_CYCLE_DELAY * 1000; // Automatically cycle widgets every X seconds, set to 0 to disable
-#else
-unsigned long m_widgetCycleDelay = 0;
-#endif
-unsigned long m_widgetCycleDelayPrev = 0;
+// Button states
+bool lastButtonOKState = HIGH;
+bool lastButtonLeftState = HIGH;
+bool lastButtonRightState = HIGH;
 
-Button buttonLeft(BUTTON_LEFT);
 Button buttonOK(BUTTON_OK);
+Button buttonLeft(BUTTON_LEFT);
 Button buttonRight(BUTTON_RIGHT);
 
 GlobalTime *globalTime; // Initialize the global time
 
 String connectingString{""};
 
-WifiWidget *wifiWidget{nullptr};
+WifiWidget *wifiWidget{ nullptr };
 
 int connectionTimer{0};
 const int connectionTimeout{10000};
 bool isConnected{true};
 
-ScreenManager *sm;
-WidgetSet *widgetSet;
-
-// This function should probably be moved somewhere else
 bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap) {
     if (y >= tft.height())
         return 0;
-    // Dim bitmap
-    for (int i = 0; i < w * h; i++) {
-        bitmap[i] = Utils::rgb565dim(bitmap[i], sm->getBrightness(), true);
-    }
     tft.pushImage(x, y, w, h, bitmap);
     return 1;
 }
 
-/**
- * The ISR handlers must be static
- */
-void isrButtonChangeLeft() { buttonLeft.isrButtonChange(); }
-void isrButtonChangeMiddle() { buttonOK.isrButtonChange(); }
-void isrButtonChangeRight() { buttonRight.isrButtonChange(); }
-
-void setupButtons() {
-    buttonLeft.begin();
-    buttonOK.begin();
-    buttonRight.begin();
-
-    attachInterrupt(digitalPinToInterrupt(BUTTON_LEFT), isrButtonChangeLeft, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(BUTTON_OK), isrButtonChangeMiddle, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(BUTTON_RIGHT), isrButtonChangeRight, CHANGE);
-}
+ScreenManager* sm;
+WidgetSet* widgetSet;
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println();
-    Serial.println("Starting up...");
 
-    TJpgDec.setSwapBytes(true); // JPEG rendering setup
-    TJpgDec.setCallback(tft_output);
-    setupButtons();
+  buttonLeft.begin();
+  buttonOK.begin();
+  buttonRight.begin();
 
-    sm = new ScreenManager(tft);
-    sm->fillAllScreens(TFT_BLACK);
-    sm->setFontColor(TFT_WHITE);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Starting up...");
 
-    sm->selectScreen(0);
-    sm->drawCentreString("Welcome", ScreenCenterX, ScreenCenterY, 29);
+  sm = new ScreenManager(tft);
+  sm->selectAllScreens();
+  sm->getDisplay().fillScreen(TFT_WHITE);
+  sm->reset();
+  widgetSet = new WidgetSet(sm);
 
-    sm->selectScreen(1);
-    sm->drawCentreString("Info Orbs", ScreenCenterX, ScreenCenterY - 50, 22);
-    sm->drawCentreString("by", ScreenCenterX, ScreenCenterY - 5, 22);
-    sm->drawCentreString("brett.tech", ScreenCenterX, ScreenCenterY + 30, 22);
-    sm->setFontColor(TFT_RED);
-    sm->drawCentreString("version: 1.1.0", ScreenCenterX, ScreenCenterY + 65, 14);
-
-    sm->selectScreen(2);
-
-    TJpgDec.setJpgScale(1);
-    TJpgDec.drawJpg(0, 0, logo_start, logo_end - logo_start);
-
-    widgetSet = new WidgetSet(sm);
+  TJpgDec.setSwapBytes(true); // jpeg rendering setup
+  TJpgDec.setCallback(tft_output);
 
 #ifdef GC9A01_DRIVER
-    Serial.println("GC9A01 Driver");
+  Serial.println("GC9A01 Driver");
+#endif
+#ifdef ILI9341_DRIVER
+  Serial.println("ILI9341 Driver");
 #endif
 #if HARDWARE == WOKWI
-    Serial.println("Wokwi Build");
+  Serial.println("Wokwi Build");
 #endif
 
-    pinMode(BUSY_PIN, OUTPUT);
-    Serial.println("Connecting to WiFi");
+  pinMode(BUSY_PIN, OUTPUT);
+  Serial.println("Connecting to: " + String(WIFI_SSID));
 
-    wifiWidget = new WifiWidget(*sm);
-    wifiWidget->setup();
+  wifiWidget = new WifiWidget(*sm);
+  wifiWidget->setup();
 
-    globalTime = GlobalTime::getInstance();
+  globalTime = GlobalTime::getInstance();
 
-    widgetSet->add(new ClockWidget(*sm));
-#ifdef PARQET_PORTFOLIO_ID
-    widgetSet->add(new ParqetWidget(*sm));
-#endif
-#ifdef STOCK_TICKER_LIST
-    widgetSet->add(new StockWidget(*sm));
-#endif
-    widgetSet->add(new WeatherWidget(*sm));
+  widgetSet->add(new ClockWidget(*sm));
+  widgetSet->add(new StockWidget(*sm));
+  widgetSet->add(new WeatherWidget(*sm));
+  widgetSet->add(new NameWidget(*sm, OWNER_NAME));
 #ifdef WEB_DATA_WIDGET_URL
-    widgetSet->add(new WebDataWidget(*sm, WEB_DATA_WIDGET_URL));
+  widgetSet->add(new WebDataWidget(*sm, WEB_DATA_WIDGET_URL));
 #endif
 #ifdef WEB_DATA_STOCK_WIDGET_URL
-    widgetSet->add(new WebDataWidget(*sm, WEB_DATA_STOCK_WIDGET_URL));
+  widgetSet->add(new WebDataWidget(*sm, WEB_DATA_STOCK_WIDGET_URL));
 #endif
-#ifdef MQTT_WIDGET_HOST
-    widgetSet->add(new MQTTWidget(*sm, MQTT_WIDGET_HOST, MQTT_WIDGET_PORT));
-#endif
-
-    m_widgetCycleDelayPrev = millis();
-}
-
-void checkCycleWidgets() {
-    if (m_widgetCycleDelay > 0 && (m_widgetCycleDelayPrev == 0 || (millis() - m_widgetCycleDelayPrev) >= m_widgetCycleDelay)) {
-        widgetSet->next();
-        m_widgetCycleDelayPrev = millis();
-    }
-}
-
-void checkButtons() {
-    // Reset cycle timer whenever a button is pressed
-    if (buttonLeft.pressedShort()) {
-        // Left short press cycles widgets backward
-        Serial.println("Left button short pressed -> switch to prev Widget");
-        m_widgetCycleDelayPrev = millis();
-        widgetSet->prev();
-    } else if (buttonRight.pressedShort()) {
-        // Right short press cycles widgets forward
-        Serial.println("Right button short pressed -> switch to next Widget");
-        m_widgetCycleDelayPrev = millis();
-        widgetSet->next();
-    } else {
-        ButtonState leftState = buttonLeft.getState();
-        ButtonState middleState = buttonOK.getState();
-        ButtonState rightState = buttonRight.getState();
-
-        // Everying else that is not BTN_NOTHING will be forwarded to the current widget
-        if (leftState != BTN_NOTHING) {
-            Serial.printf("Left button pressed, state=%d\n", leftState);
-            m_widgetCycleDelayPrev = millis();
-            widgetSet->buttonPressed(BUTTON_LEFT, leftState);
-        } else if (middleState != BTN_NOTHING) {
-            Serial.printf("Middle button pressed, state=%d\n", middleState);
-            m_widgetCycleDelayPrev = millis();
-            widgetSet->buttonPressed(BUTTON_OK, middleState);
-        } else if (rightState != BTN_NOTHING) {
-            Serial.printf("Right button pressed, state=%d\n", rightState);
-            m_widgetCycleDelayPrev = millis();
-            widgetSet->buttonPressed(BUTTON_RIGHT, rightState);
-        }
-    }
 }
 
 void loop() {
-    if (wifiWidget->isConnected() == false) {
-        wifiWidget->update();
-        wifiWidget->draw();
-        widgetSet->setClearScreensOnDrawCurrent(); // Clear screen after wifiWidget
-        delay(100);
-    } else {
-        if (!widgetSet->initialUpdateDone()) {
-            widgetSet->initializeAllWidgetsData();
-        }
-        globalTime->updateTime();
-
-        checkButtons();
-
-        widgetSet->updateCurrent();
-        widgetSet->updateBrightnessByTime(globalTime->getHour24());
-        widgetSet->drawCurrent();
-
-        checkCycleWidgets();
+  if (wifiWidget->isConnected() == false) {
+    wifiWidget->update();
+    wifiWidget->draw();
+    widgetSet->setClearScreensOnDrawCurrent(); //clear screen after wifiWidget
+    delay(100);
+  } else {
+    if (!widgetSet->initialUpdateDone()) {
+      widgetSet->initializeAllWidgetsData();
     }
+    globalTime->updateTime();
+
+    if (buttonLeft.pressed()) {
+      Serial.println("Left button pressed");
+      widgetSet->prev();
+    }
+    if (buttonOK.pressed()) {
+      Serial.println("OK button pressed");
+      widgetSet->changeMode();
+    }
+    if (buttonRight.pressed()) {
+      Serial.println("Right button pressed");
+      widgetSet->next();
+    }
+
+    widgetSet->updateCurrent();
+    widgetSet->drawCurrent();
+  }
 }
